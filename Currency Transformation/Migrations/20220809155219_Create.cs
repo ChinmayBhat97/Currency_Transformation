01ebf700e6017e2eb56923770104fbf5659ffa2f
CHINMAY BHAT <chinmaybhat29@gmail.com>
2022-08-15 11:23:51 +0530

    Add project files.

---
+++ Currency Transformation/Migrations/20220809155219_Create.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Currency_Transformation.Migrations
+{
+    public partial class Create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountNumber = table.Column<string>(type: "nvarchar(15)", nullable: false),
+                    BeneficiaryName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    BankName = table.Column<string>(type: "nvarchar(25)", nullable: false),
+                    swiftcode = table.Column<string>(type: "nvarchar(15)", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
---
+++ Currency Transformation/Models/Transaction.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Currency_Transformation.Models
+{
+    public class Transaction
+    {
+        [Key]
+        [DisplayName("Transaction ID")]
+        public int TransactionId  { get; set; }
+
+        [Column(TypeName="nvarchar(15)")]
+        [DisplayName("Account Number")]
+        [Required(ErrorMessage ="This field is required")]
+        public  string  AccountNumber{ get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        [DisplayName("Beneficiary Name")]
+        [Required(ErrorMessage = "This field is required")]
+        public string BeneficiaryName { get; set; }
+        [Column(TypeName = "nvarchar(25)")]
+        [DisplayName("Bank Name")]
+        [Required(ErrorMessage = "This field is required")]
+        public string BankName { get; set; }
+        [Column(TypeName = "nvarchar(15)")]
+        [DisplayName("IFSC")]
+        [Required(ErrorMessage = "This field is required")]
+        public string swiftcode { get; set; }
+        [DisplayName("Amount")]
+        [Required(ErrorMessage = "This field is required")]
+        public int Amount { get; set; }
+        [DisplayName("Date & Time")]
+        
+        public DateTime Date { get; set; }
+    }
+}
